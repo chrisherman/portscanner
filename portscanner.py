4c9e13226a0e8de98d95839c7a35f1c8cf9f76a6
chrisherman <chrisherman@users.noreply.github.com>
2023-10-07 12:47:09 +0100

    Add files via upload

--- portscanner.py
+++ portscanner.py
@@ -1,91 +1,91 @@
-import pyfiglet, os
+import sys
+import socket
+import os
+import pyfiglet
+
 print(pyfiglet.figlet_format("PORT SCANNER", font="slant"))
 
-import socket
-import sys
-import webbrowser
-from concurrent.futures import ThreadPoolExecutor
-
-def test_open_port(ip, port, timeout=1):
-    """Test if port is open."""
+def test_open_port(ip, port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.settimeout(timeout)
-        try:
-            s.connect((ip, port))
-            return True
-        except (socket.timeout, socket.error):
-            return False
-
-def generate_html_file(ip_port_mapping, original_cmd):
-    """Generate an HTML file with the IPs and ports as clickable links."""
-    with open("output.html", "w") as file:
-        file.write("<html><body>")
-        file.write(f"<p><strong>{original_cmd}</strong></p><br/>")
-        for ip, ports in ip_port_mapping.items():
-            file.write(f"{ip}: ")
-            for port in ports:
-                link = f"http://{ip}:{port}"
-                file.write(f'<a href="{link}" target="_blank">{port}</a> ')
-            file.write('<br/>')
-        file.write("</body></html>")
-
+        s.settimeout(1)
+        result = s.connect_ex((ip, port))
+        return result == 0
 
 def generate_ips(target):
-    """Generate a list of IPs based on the provided target."""
     if '*' in target:
-        base_ip = target.split('.')[0:3]
-        return [f"{'.'.join(base_ip)}.{i}" for i in range(1, 256)]
+        base_ip = target.replace('*', '{}')
+        return [base_ip.format(i) for i in range(1, 256)]
     elif '-' in target:
-        base_ip, ip_range = target.rsplit('.', 1)
-        start, end = map(int, ip_range.split('-'))
-        return [f"{base_ip}.{i}" for i in range(start, end+1)]
+        base_ip, range_end = target.split('-')
+        base_parts = base_ip.split('.')
+        start = int(base_parts[-1])
+        end = int(range_end.split('.')[-1])
+        return ['.'.join(base_parts[:-1] + [str(i)]) for i in range(start, end + 1)]
     else:
         return [target]
 
-def generate_ports(port_arg):
-    """Generate a list of ports based on the provided argument."""
-    if ',' in port_arg:
-        return list(map(int, port_arg.split(',')))
-    elif '-' in port_arg:
-        start, end = map(int, port_arg.split('-'))
-        return list(range(start, end+1))
+def generate_html_file(open_ports, cmd):
+    with open("output.html", "w") as f:
+        f.write("<html><head><title>Open Ports</title></head><body>")
+        f.write(f"<h2>Command: {cmd}</h2>")
+        for ip, ports in open_ports.items():
+            try:
+                hostname = socket.gethostbyaddr(ip)[0]
+                display_name = f"{ip} ({hostname})" if hostname != ip else ip
+            except socket.herror:
+                display_name = ip
+            for port in ports:
+                f.write(f"<p><a href='http://{ip}:{port}' target='_blank'>{display_name}:{port}</a></p>")
+        f.write("</body></html>")
+
+
+def parse_ports(ports_str):
+    ports = []
+    if "," in ports_str:
+        for part in ports_str.split(","):
+            if "-" in part:
+                start, end = map(int, part.split("-"))
+                ports.extend(range(start, end + 1))
+            else:
+                ports.append(int(part))
+    elif "-" in ports_str:
+        start, end = map(int, ports_str.split("-"))
+        ports.extend(range(start, end + 1))
     else:
-        return [int(port_arg)]
+        ports.append(int(ports_str))
+    
+    return ports
 
 def main(target, ports):
     open_ports = {}
     ips = generate_ips(target)
+    total_ips = len(ips)
 
-    # Scan IPs for open ports using threads for faster scanning
+    progress_count = 0
+
     for ip in ips:
-        open_ports_for_ip = []
+        open_ports[ip] = []
         for port in ports:
+            sys.stdout.write(f"\rScanning {ip}:{port}...                 ")  # Pad to overwrite longer lines
+            sys.stdout.flush()
+            
             if test_open_port(ip, port):
-                print(f"Port {port} open on {ip}")
-                open_ports_for_ip.append(port)
-        if open_ports_for_ip:
-            open_ports[ip] = open_ports_for_ip
+                open_ports[ip].append(port)
+                sys.stdout.write(f"\rPort {port} open on {ip}\n")
+        
+        progress_count += 1
 
-    # Generate an HTML file with open port IPs
     if open_ports:
         original_cmd = ' '.join(sys.argv)
         generate_html_file(open_ports, original_cmd)
-
-        # Open the generated HTML file in the default web browser
         os.system('open output.html')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Usage: python script_name.py <IP_address_or_range_or_wildcard> [port_or_range_or_list]")
-        print("\nExamples:")
-        print("  python script_name.py 192.168.1.10")
-        print("  python script_name.py 192.168.1.* 80-85")
-        print("  python script_name.py 192.168.1.45-67 80,81,82")
+        print("Usage: script.py [IP or IP range with * or -] [optional ports or port ranges separated by ,]")
         sys.exit(1)
+    
+    target_ip = sys.argv[1]
+    target_ports = parse_ports(sys.argv[2]) if len(sys.argv) > 2 else [80]
 
-    target = sys.argv[1]
-    ports_arg = sys.argv[2] if len(sys.argv) > 2 else "80"
-    ports = generate_ports(ports_arg)
-
-    main(target, ports)
-
+    main(target_ip, target_ports)
